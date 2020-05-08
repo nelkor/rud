cf9574d0e160910a173cfc46c42b2144bd5b580c
Nelkor <nelkor9855118751@gmail.com>
2020-05-08 15:51:16 +0300

    types

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare function rudDigits(
+    ymd: [Number, String],
+    glue?: String,
+): String;
+
+declare function rudString(
+    ymd: [Number, String],
+    writeYear?: Boolean,
+): String;
+
+declare function rudInterval(
+    d1: [Number, String],
+    d2: [Number, String],
+): String;
